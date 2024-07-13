f952f67db97ef7f3a9a8b56366b3981822d578a0
SayulRamirez <sayulramirez@gmail.com>
2024-07-13 11:20:50 -0600

    Agregado el caso de prueba para obtener todas las tareas

--- src/test/java/com/metaphorce/api/tarea/service/TareaServiceTest.java
+++ src/test/java/com/metaphorce/api/tarea/service/TareaServiceTest.java
@@ -6,7 +6,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -32,5 +35,22 @@ public class TareaServiceTest {
         Tarea capturedTarea = tareaArgumentCaptor.getValue();
 
         assertEquals(tarea, capturedTarea);
+    }
+
+    @Test
+    void getTareas() {
+        // Dado
+        Tarea tarea1 = new Tarea(null, "Tarea 1", false);
+        Tarea tarea2 = new Tarea(null, "Tarea 2", true);
+
+        List<Tarea> tareas = List.of(tarea1, tarea2);
+        when(tareaRepository.findAll()).thenReturn(tareas);
+
+        // Cuando
+        List<Tarea> result = underTest.getTareas();
+
+        // Entonces
+        verify(tareaRepository, times(2)).findAll();
+        assertEquals(tareas, result);
     }
 }
